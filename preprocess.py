a0a63b4c63425d9133384a81dda214c97cca935f
Rishub Tamirisa <rishubt2@illinois.edu>
2022-06-23 17:07:35 -0700

    finished FOBIE preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,69 +1,78 @@
+'''
+    preprocess - this code preprocess the FOBIE dataset into a format compatible with the HF transformers API
+        Procedure:
+            - iterate through the FOBIE json dataset and extract the sentences and spans
+            - adjust spans to match the tokenization of the sentence and padding of the tokenizer
+    Author: Rishub Tamirisa (rishub.tamirisa@nasa.gov / rishubt2@illinois.edu)
+'''
+
 import json
 import torch
 import numpy as np
-import pandas as pd
-from transformers import AdamW, AutoTokenizer
+from transformers import AutoTokenizer
+import tokenizations
+
+def match_span_to_tokenizer(ground, split, span):
+    """Converts a span to the tokenizer's tokenization.
+    Returns:
+        (list): list containing:
+            [int, int]: span in tokenizer's tokenization
+    """
+    a2b, b2a = tokenizations.get_alignments(ground, split)
+    adjusted = []
+    for idx, val in enumerate(b2a):
+        if val[0] in range(span[0], span[1]+1):
+            adjusted.append(idx)
+    if adjusted:
+        return [adjusted[0]+1, adjusted[len(adjusted)-1]+1]
+    else:
+        return None
+
+def FOBIE_preprocess(checkpoint, tokenizer, data):
+    """Prepares the FOBIE dataset for the HF transformers API.
+    Returns:
+        (dict): dict containing:
+            'attention_mask': (torch.tensor),
+            'input_ids': (torch.tensor),
+            'token_type_ids': (torch.tensor),
+            'labels': (torch.tensor)
+    """
+    dict = {
+            'Train': {
+                'sentences':[],
+                'labels':[]
+            }
+        }
+    sentences = []
+    for source_doc_id in data:
+        for sentence_id in data[source_doc_id]:
+            sentence: str = data[source_doc_id][sentence_id]['sentence']
+            sentences.append(sentence)
+
+    batch = tokenizer(sentences, padding=True, truncation=False, return_tensors='pt')
+
+    for source_doc_id in data:
+        for sentence_id in data[source_doc_id]:
+            sentence: str = data[source_doc_id][sentence_id]['sentence']
+            tokens = tokenizer.tokenize(sentence)
+            label = np.zeros(len(batch['input_ids'][0]))
+            for sentence_modifier_id in data[source_doc_id][sentence_id]['annotations']['modifiers']:
+                for arg_id in data[source_doc_id][sentence_id]['annotations']['modifiers'][sentence_modifier_id]:
+                    arg_attributes = data[source_doc_id][sentence_id]['annotations']['modifiers'][sentence_modifier_id][arg_id]
+                    span = [int(arg_attributes['span_start']), int(arg_attributes['span_end'])]
+                    span = match_span_to_tokenizer(sentence.split(), tokens, span)
+                    if span:
+                        label[span[0]:span[1]] = 1
+            dict['Train']['sentences'].append(tokens)
+            dict['Train']['labels'].append(label)
+    dict['Train']['labels'] = np.array(dict['Train']['labels'])
+    batch['labels'] = torch.tensor(dict['Train']['labels'])
+
+    return batch
 
 
 checkpoint = "allenai/scibert_scivocab_cased"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-
-# FOBIE Dataset
 data = json.load(open("data/dev_set.json"))
 
-#   1   2 3    4     5  6     7
-# this is a testing of the spanning
-#   1   2 3   4   5  6   7   8    9
-# this is a test ing of the span ing
-
-def match_span_to_tokenizer(ground, split, span):
-    gd_idx = 0
-    sp_idx = 0
-    ground.append("[END]")
-    split.append("[END]")
-    result = [span[0], span[1]]
-    shift = None
-    while gd_idx < len(ground):
-        if gd_idx < len(ground) - 1 and ground[gd_idx] != split[sp_idx]:
-            gd_idx += 1
-            while (sp_idx < len(split) and split[sp_idx] != ground[gd_idx]):
-                sp_idx += 1
-            shift = sp_idx - gd_idx
-            if gd_idx - 1 <= span[1]:
-                result[1] = span[1] + shift
-            if gd_idx - 1 <= span[0]:
-                result[0] = span[0] + shift
-        sp_idx += 1
-        gd_idx += 1
-    return result
-
-
-ground = ['Another', 'interesting', 'finding', 'was', 'the', 'lack', 'of', 'correlation', 'between', 'resting', 'levels', 'of', 'both', 'HSP', '90β', 'and', 'HSC', '70', 'and', 'CTmax.']
-split = ['another', 'interesting', 'finding', 'was', 'the', 'lack', 'of', 'correlation', 'between', 'resting', 'levels', 'of', 'both', 'hs', '##p', '90', '##β', 'and', 'hs', '##c', '70', 'and', 'ct', '##max', '.']
-span = [1,3] 
-span = match_span_to_tokenizer(ground, split, span)
-# print(span)       
-        
-dict = {
-        'Train': {
-            'sentences':[],
-            'labels':[]
-        }
-       }
-dict['Train']['sentences'].append('test')
-print(dict)
-
-for source_doc_id in data:
-    for sentence_id in data[source_doc_id]:
-        sentence: str = data[source_doc_id][sentence_id]['sentence']
-        tokens = tokenizer.tokenize(sentence)
-        for sentence_modifier_id in data[source_doc_id][sentence_id]['annotations']['modifiers']:
-            label = np.zeros(len(tokens))
-            for arg_id in data[source_doc_id][sentence_id]['annotations']['modifiers'][sentence_modifier_id]:
-                arg_attributes = data[source_doc_id][sentence_id]['annotations']['modifiers'][sentence_modifier_id][arg_id]
-                span = [int(arg_attributes['span_start']), int(arg_attributes['span_end'])]
-                print(arg_attributes)
-
-                # span = match_span_to_tokenizer(sentence.lower().split(), tokens, span)
-                # label[span[0]:span[1]] = 1
-            print(label)
+batch = FOBIE_preprocess(checkpoint, tokenizer, data)
